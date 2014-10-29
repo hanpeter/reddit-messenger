89e823dcc56d3769d6cd4680c40d73b6ce1b8be2
Peter Han <han.peter.621@gmail.com>
2014-10-28 21:35:06 -0700

    Minor window changes

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('index.html', {
-        bounds: {
+        innerBounds: {
             width: 800,
             height: 500
+        },
+        frame: {
+            type: 'chrome'
         }
     });
 });
