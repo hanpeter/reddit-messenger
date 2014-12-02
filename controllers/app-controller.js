f2f3cb7e9768f31d5b5b7b9c4308c0a4064585aa
Peter Han <phan@clicktime.com>
2014-12-01 16:11:21 -0800

    Clean up unused code

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -1,45 +1,41 @@
-App.controller('AppController', ['$scope', 'RedditService', 'RedditConfig', 'ThreadFactoryService', 'NotificationService', function ($scope, RedditService, RedditConfig, ThreadFactoryService, NotificationService) {
-    var notificationID = '',
-        NOTIFICATION_BUFFER = 15000,
-        NOTIFICATION_SNOOZE = 30000,
-        notificationRefreshTime = moment();
-
-    function checkUnreadMessages() {
-        $scope.sync(function () {
-            ThreadFactoryService.checkUnreadMessages()
-                .done(NotificationService.update);
-        });
-    }
-
-    _.extend($scope, {
-        messages: ThreadFactoryService.getThreads(),
-        activeThread: null,
-        sync: function (callback) {
-            if ($scope.$$phase) {
-                callback();
-            }
-            else {
-                $scope.$apply(callback);
-            }
-        },
-        setActiveThread: function (thread) {
-            $scope.activeThread = thread;
-        },
-        updateMessages: function () {
-            var activeThreadID = $scope.activeThread ? $scope.activeThread.threadID : undefined;
-
-            ThreadFactoryService.updateThreads().done(function (threads) {
-                $scope.sync(function () {
-                    $scope.messages = threads;
-                    $scope.activeThread = _.find(threads, function (thread) { return thread.threadID === activeThreadID; });
-                });
+App.controller('AppController', ['$scope', 'RedditService', 'RedditConfig', 'ThreadFactoryService', 'NotificationService', 
+    function ($scope, RedditService, RedditConfig, ThreadFactoryService, NotificationService) {
+        function checkUnreadMessages() {
+            $scope.sync(function () {
+                ThreadFactoryService.checkUnreadMessages()
+                    .done(NotificationService.update);
             });
         }
-    });
 
-    RedditService.getToken()
-        .done(function () {
-            $scope.updateMessages()
-            setInterval(checkUnreadMessages, 5000);
+        _.extend($scope, {
+            messages: ThreadFactoryService.getThreads(),
+            activeThread: null,
+            sync: function (callback) {
+                if ($scope.$$phase) {
+                    callback();
+                }
+                else {
+                    $scope.$apply(callback);
+                }
+            },
+            setActiveThread: function (thread) {
+                $scope.activeThread = thread;
+            },
+            updateMessages: function () {
+                var activeThreadID = $scope.activeThread ? $scope.activeThread.threadID : undefined;
+
+                ThreadFactoryService.updateThreads().done(function (threads) {
+                    $scope.sync(function () {
+                        $scope.messages = threads;
+                        $scope.activeThread = _.find(threads, function (thread) { return thread.threadID === activeThreadID; });
+                    });
+                });
+            }
         });
-}]);
+
+        RedditService.getToken()
+            .done(function () {
+                $scope.updateMessages()
+                setInterval(checkUnreadMessages, 5000);
+            });
+    }]);
